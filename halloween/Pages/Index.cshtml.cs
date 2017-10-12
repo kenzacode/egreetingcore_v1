57ccfc570b16b3c2d4a626a284180f80086a70c2
Mia <nezhma@yahoo.com>
2017-10-11 20:01:38 -0700

    .

--- halloween/Pages/Index.cshtml.cs
+++ halloween/Pages/Index.cshtml.cs
@@ -8,10 +8,27 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace halloween.Pages
 {
     public class IndexModel : PageModel
-    {
+    {   [BindProperty]
+        public string name { get; set; }
+        [BindProperty]
+        public string sendto { get; set; }
+        [BindProperty]
+        public string from { get; set; }
+        [BindProperty]
+        public string subject { get; set; }
+        [BindProperty]
+        public string message { get; set; }
+        
+
         public void OnGet()
         {
 
         }
+
+        public void OnPost()
+        {
+            
+        }
     }
 }
+
