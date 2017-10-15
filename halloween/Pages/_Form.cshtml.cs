544641d850c5c1d0ad2b28ffd9d4a3ab0c0ff43d
Mia <nezhma@yahoo.com>
2017-10-14 18:40:00 -0700

    .

--- halloween/Pages/_Form.cshtml.cs
+++ halloween/Pages/_Form.cshtml.cs
@@ -1,22 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-
 namespace halloween.Pages
 {
-    public class IndexModel : PageModel
+    public class _FormModel : PageModel
     {
-
         public void OnGet()
         {
-
         }
-
     }
-
 }
-
